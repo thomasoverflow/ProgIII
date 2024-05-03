9d38665fe8e88093fa9dd0362e543a6d962cd231
ThomasThinkpad <thomascarmo@outlook.com>
2024-05-03 14:31:35 -0300

    Aulas com exercícios 12,13,14 finlizados...falta Ex11

--- Ex_sala/Ex11/Espera.h
+++ Ex_sala/Ex11/Espera.h
@@ -4,14 +4,30 @@
 #include <string.h>
 
 using namespace std;
+
+template <template <typename> class T, typename G>
 // Implemente sua lista de espera
 class Espera{
     string disciplina;
     int tamanho;
-    string *nomes;
+    T<G> *nomes;
 public:
+    Espera(string pNome, int pMax){
+        disciplina = pNome;
+        tamanho = pMax;
+        nomes = new T<G>[pMax];
+    }
+
     void inserir_aluno(std::string nome){
 
+    }
+
+    void mostrar_lista(){
+
+    }
+
+    ~Espera(){
+        delete nomes;
     }
 };
 #endif /* _ESPERA_H */
